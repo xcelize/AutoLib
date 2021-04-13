f4c615b73adc5e9f5085a59966e43cf2912b5a9f
Alexis <apetraz@cilog.fr>
2021-04-13 20:46:34 +0200

    Commit change structures

---
+++ Autolib/Controllers/StationController.cs
@@ -0,0 +1,62 @@
+﻿using Autolib.Modeles.DAO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Autolib.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StationController : ControllerBase
+    {
+
+        // GET: api/<StationController>
+        [HttpGet]
+        public async Task<IEnumerable<Station>> Get()
+        {
+            var stations = await StationService.getInstance().getAll();
+            return stations;
+        }
+
+        // GET api/<StationController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            Station station = await StationService.getInstance().getStation(id);
+            if (station == null)
+            {
+                return NotFound();
+            }
+            return Ok(station);
+        }
+
+        // POST api/<StationController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Station value)
+        {
+            Station station = await StationService.getInstance().postStation(value);
+            if (station == null)
+            {
+                return NotFound();
+            }
+            return Ok(station);      
+        }
+
+        // PUT api/<StationController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<StationController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
