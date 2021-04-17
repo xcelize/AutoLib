eaf56071acbc9fb9566ea154e8bcf581a2d253b2
Vinh Phuc TRUONG <48383383+vinhphuctr@users.noreply.github.com>
2021-04-17 22:54:10 +0200

    modifier dialogue stations

--- Autolib/ClientApp/src/app/dialogue/dialogue.component.ts
+++ Autolib/ClientApp/src/app/dialogue/dialogue.component.ts
@@ -1,6 +1,7 @@
 import { Inject } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Vehicule } from '../models/vehicule';
 import { MockStationServiceService } from '../services/mock-station-service.service';
 
 
@@ -11,12 +12,16 @@ import { MockStationServiceService } from '../services/mock-station-service.serv
 })
 export class DialogueComponent implements OnInit {
 
+  showBtnReserver: boolean = false;
+  showInfo: boolean = false;
+
   constructor(
     @Inject(MAT_DIALOG_DATA) public data,
     private _mockStationService: MockStationServiceService
   ) { }
 
   ngOnInit() {
+    this.showElement();
   }
 
   public totalBornes(): number { 
@@ -26,7 +31,7 @@ export class DialogueComponent implements OnInit {
   public totalBornesDispo(): number {
     let dispo = 0;
     for (let borne of this.data.bornes) {
-      if (borne.etatBorne == 1) dispo++;
+      if (borne.etatBorne == 0) dispo++;
     }
     return dispo;
   }
@@ -36,22 +41,52 @@ export class DialogueComponent implements OnInit {
       this.data.adresse + ", " +
       this.data.code_postal.toString() + " " + this.data.ville;
   }
-
+  /*
   public getBackgroundColor(etatBorne: number): string {
     let color = 'rgb(220,220,220)';
-    if (etatBorne == 0) color = "rgb(51, 174, 103)";
+
+    if (etatBorne == 0) {
+      if (dispoVehicule == 'LIBRE') {
+        color = "rgb(51, 174, 103)";
+      }
+      else {
+        color = "rgb(0, 148, 255)";
+      }
+    }
+    return color;
+  }*/
+
+  public getBackgroundColor(vehicule: Vehicule): string {
+    let color = 'rgb(220,220,220)';
+    if (vehicule != undefined) {
+      if (vehicule.disponibilite == 'LIBRE') color = "rgb(51, 174, 103)";
+      else color = "rgb(0, 148, 255)";
+    }
     return color;
   }
-
+  /*
   public getColor(etatBorne: number): string {
     let color = 'black';
     if (etatBorne == 0) color = 'white';
     return color;
+  }*/
+
+  public getColor(vehicule: Vehicule): string {
+    let color = 'black';
+    if (vehicule != undefined) color = 'white';
+    return color;
   }
 
-  sendData(): void {
-    this._mockStationService.getStation(this.data.id).subscribe(
-      (data) => { console.log(data); }
-    )
+  public showElement(): void {
+    let dispo = this.totalBornesDispo();
+    if (dispo == 0) {
+      this.showBtnReserver = false;
+      this.showInfo = true;
+    }
+    else {
+      this.showBtnReserver = true;
+      this.showInfo = false;
+    }
   }
+
 }
--- Autolib/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ Autolib/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class NavMenuComponent {
   isExpanded = false;
+  hide = true;
 
   collapse() {
     this.isExpanded = false;
