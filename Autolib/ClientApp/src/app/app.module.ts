d58e2b1d6628d9804ed3275b78605c89c084d920
Vinh Phuc TRUONG <48383383+vinhphuctr@users.noreply.github.com>
2021-04-23 03:25:37 +0200

    finaliser le formulaire inscription

--- Autolib/ClientApp/src/app/app.module.ts
+++ Autolib/ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
@@ -11,6 +11,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatTableModule } from '@angular/material/table';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MAT_DATE_LOCALE } from '@angular/material';
 
 
 import { AppComponent } from './app.component';
@@ -67,9 +68,12 @@ import { MatNativeDateModule } from '@angular/material';
     MatDialogModule,
     MatTableModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
+  ],
   bootstrap: [AppComponent],
   entryComponents: [
     DialogueComponent,
--- Autolib/ClientApp/src/app/inscription/inscription.component.ts
+++ Autolib/ClientApp/src/app/inscription/inscription.component.ts
@@ -1,15 +1,77 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, FormGroup, Validators, AbstractControl, ValidatorFn, ValidationErrors, FormGroupDirective, NgForm, Form } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material';
+import { Client } from '../models/client';
+
+class CrossFieldErrorMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl, form: FormGroupDirective | NgForm): boolean {
+    return control.dirty && form.invalid;
+    }
+}
 
 @Component({
   selector: 'app-inscription',
   templateUrl: './inscription.component.html',
-  styleUrls: ['./inscription.component.css']
+  styleUrls: ['./inscription.component.css'],
 })
-export class InscriptionComponent implements OnInit {
 
-  constructor() { }
+
+export class InscriptionComponent implements OnInit {
+  currentDate = new Date();
+  minLen: number = 8;
+  hide: boolean = true;
+  inscriptionForm: FormGroup;
+  errorMatcher = new CrossFieldErrorMatcher();
+  newClient: Client = new Client();
+
+  constructor(private fb: FormBuilder) {
+    this.inscriptionForm = this.fb.group({
+      nom: ['', [Validators.required]],
+      prenom: ['', [Validators.required]],
+      date: ['', [Validators.required]],
+      id: ['', [Validators.required, Validators.minLength(this.minLen)]],
+      mdp: ['', [Validators.required, Validators.minLength(this.minLen)]],
+      confirmMdp: ['', [Validators.required]]
+    },
+      { validators: this.passwordMatchValidator }
+    );
+  }
 
   ngOnInit() {
+  }
+
+
+  passwordMatchValidator(g: FormGroup) {
+    const mdp = g.get('mdp').value;
+    const confirm = g.get('confirmMdp').value;
+    return mdp !== confirm ? { mismatch: true } : null ;
+  }
+
+
+  isRequired(attr: string): boolean {
+    if (this.inscriptionForm.get(attr).hasError('required')) return true;
+    else return false;
+  }
+
+  isPasswordMatched(): boolean {
+    if (this.inscriptionForm.hasError('mismatch')) return true;
+    else return false;
+  }
+
+  getNom(): string { return this.inscriptionForm.get('nom').value; }
+  getPrenom(): string { return this.inscriptionForm.get('prenom').value; }
+  getDateNaiss(): Date { return this.inscriptionForm.get('date').value; }
+  getLogin(): string { return this.inscriptionForm.get('id').value; }
+  getMdp(): string { return this.inscriptionForm.get('mdp').value; }
+
+
+  onSubmit() {
+    this.newClient.nom = this.getNom();
+    this.newClient.prenom = this.getPrenom();
+    this.newClient.date_naissance = this.getDateNaiss();
+    this.newClient.login = this.getLogin();
+    this.newClient.mdp = this.getMdp();
+    console.log(this.newClient);
   }
 
 }
